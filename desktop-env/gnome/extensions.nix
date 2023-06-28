ecdf4b766d00bc1f52170904c707860aa5a9e18b
its-ag <arunavagh@outlook.com>
2023-06-28 19:12:04 +0530

    fav apps

--- desktop-env/gnome/extensions.nix
+++ desktop-env/gnome/extensions.nix
@@ -12,11 +12,22 @@
         "appindicatorsupport@rgcjonas.gmail.com"
         "blur-my-shell@aunetx"
       ];
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "brave-browser.desktop"
+        "code.desktop"
+        "org.gnome.Console.desktop"
+        "discord.desktop"
+        "org.gnome.Calendar.desktop"
+        "org.gnome.Settings.desktop"
+        "org.gnome.TextEditor.desktop"
+        "org.gnome.Calculator.desktop"
+      ];
     };
 
     "org/gnome/shell/extensions/appindicator" = {
       icon-saturation = 2.7755575615628914;
-      icon-size = 0;
+      icon-size = 22;
       legacy-tray-enabled = false;
     };
 
