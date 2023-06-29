52b8c8ce27c5705993855544167196cd5ff6857d
its-ag <arunavagh@outlook.com>
2023-06-29 09:30:36 +0530

    dns

--- nixos/laptop-gnome/home.nix
+++ nixos/laptop-gnome/home.nix
@@ -1,21 +1,35 @@
-{ username, ... }:
+{ pkgs, username, ... }:
 
 {
   imports = [
     ../../desktop-env/gnome/home.nix
   ]
   ++ map (program: ../../home-manager/${program}.nix)
-    [ "zsh" "git" "obs-studio"];
+    [ "zsh" "git"];
 
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
+    packages = with pkgs; [
+      brave
+      rustup
+      celluloid
+      nodePackages_latest.pnpm
+      telegram-desktop
+      obs-studio
+      neofetch
+      nodejs
+      python3
+    ];
     stateVersion = "23.05";
   };
 
   programs = {
-    vscode.enable = true;
     neovim.enable = true;
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
   };
   services.gpg-agent.enable = true;
 }
